ef73b9db6265c6ef4f16d1c251e16a8085db44e9
ud39 <ud-gaming@gmx.de>
2023-12-09 14:38:45 +0100

    Add: Extraction of numbers and symbols and their coordinates

--- test/Main.hs
+++ test/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import MyLib
 import ReadInput
+import Data.Char (isDigit, digitToInt)
 
 {-
 import Test.Tasty
@@ -24,20 +25,51 @@ tests = testGroup "MyLib Tests" [faultyTest, correctTest]
 -}
 
 
+newtype Coordinates = Coordinates (String, (Int, Int)) deriving Show
+
+extractNumberCoordinates :: String -> [Coordinates]
+extractNumberCoordinates row =
+  let go :: String -> Int -> Int -> [Coordinates] -> [Coordinates]
+      go [] _ _ coords = reverse coords
+      go (c:cs) rowIdx colIdx coords
+        | c == '.' = go cs rowIdx (colIdx + 1) coords
+        | isDigit c =
+            let (digits, rest) = span isDigit (c:cs)
+                end = colIdx + length digits - 1
+                coords' = Coordinates (digits, (colIdx, end)) : coords
+            in go rest rowIdx (end + 1) coords'
+        | otherwise = go cs rowIdx (colIdx + 1) coords
+
+      processRow :: String -> Int -> [Coordinates]
+      processRow newRow rowIdx = go newRow rowIdx 0 []
+
+  in processRow row 0
+
+
+
+extractSymbolsCoordinates :: String -> [(Int, Int)]
+extractSymbolsCoordinates row =
+  let go :: String -> Int -> Int -> [(Int, Int)] -> [(Int, Int)]
+      go [] _ _ coords = reverse coords
+      go (c:cs) rowIdx colIdx coords
+        | c == '.' = go cs rowIdx (colIdx + 1) coords
+        | not (isDigit c) =
+            let coords' = (rowIdx, colIdx)
+            in go cs rowIdx (colIdx + 1) (coords' : coords)
+        | otherwise = go cs rowIdx (colIdx + 1) coords
+
+      processRow :: String -> Int -> [(Int, Int)]
+      processRow newRow rowIdx = go newRow rowIdx 0 []
+
+  in processRow row 0
+
+
 main :: IO ()
 main = do
-  games <- readFileAsLines  "./input/2023/day2.txt"
-  let parsedGames = map parseGame games
-      playedGames = map tail parsedGames
-      gamesIds = getIndexes parsedGames
-      availableCounts = ["12 red","13 green","14 blue"]
+  schematic <- readFileAsLines "./input/2023/day3.txt"
+  let exampleInput = ["467..114..","...*......","..35..633.","......#...","617*......",".....+.58.","..592.....","......755.","...$.*....",".664.598.."]
+      extractedNumbers = map extractNumberCoordinates exampleInput
+      extractedSymbols = map extractSymbolsCoordinates exampleInput
+  print extractedNumbers
+  print extractedSymbols
 
-      checkedGames = map (map (compareCounts availableCounts)) playedGames
-      allValid = map (all and) checkedGames
-      numberOfValidGames = countValidGames allValid gamesIds
-      minCubesNeed = sum (map (product . calcMinCubesNeed availableCounts) playedGames)
-
-  
-
-  print numberOfValidGames
-  print minCubesNeed
