06efce81552ec2184081f02fd0d32a543b34f1c1
ud39 <ud-gaming@gmx.de>
2023-12-10 14:26:11 +0100

    Add: Solution for part 2 day 3

--- test/Main.hs
+++ test/Main.hs
@@ -70,6 +70,23 @@ extractSymbolsCoordinates row =
   in processRow row 0
 
 
+extractedStarsCoordinates :: String -> [(Int, Int)]
+extractedStarsCoordinates row =
+  let go :: String -> Int -> Int -> [(Int, Int)] -> [(Int, Int)]
+      go [] _ _ coords = reverse coords
+      go (c:cs) rowIdx colIdx coords
+        | c == '*' =
+            let coords' = (rowIdx, colIdx)
+            in go cs rowIdx (colIdx + 1) (coords' : coords)
+        | otherwise = go cs rowIdx (colIdx + 1) coords
+
+      processRow :: String -> Int -> [(Int, Int)]
+      processRow newRow rowIdx = go newRow rowIdx 0 []
+
+  in processRow row 0
+
+
+
 checkIfNumberAdjacent :: Int -> Coordinates -> [[(Int, Int)]] -> Int
 checkIfNumberAdjacent index currentNumber symbol
     | index == 0 && check (getCoord currentNumber) symbolsElementAtIndexPlusOne = getNumber currentNumber
@@ -89,31 +106,45 @@ checkIfNumberAdjacent index currentNumber symbol
         check :: (Int, Int) -> [(Int, Int)] -> Bool
         check numberCoordinates symbolCoordinates =
             let (numberX, numberY) = numberCoordinates
-            in any (\(symX, symY) -> abs (numberY - symY) <= 1 || abs (numberX - symY) <= 1) symbolCoordinates
+            in any (\(_, symY) -> abs (numberY - symY) <= 1 || abs (numberX - symY) <= 1) symbolCoordinates
 
+calcGearRatio :: Int -> (Int, Int) -> [[Coordinates]] -> [Int]
+calcGearRatio index symbol coordinates
+    | index == 0 = check symbol (coordinatesElementAtIndexPlusOne ++ coordinatesElementAtIndex)
+    | index == length coordinates - 1 = check symbol (coordinatesElementAtIndexMinusOne ++ coordinatesElementAtIndex)
+    | index > 0 && index < length coordinates - 1 = check symbol (coordinatesElementAtIndex ++ coordinatesElementAtIndexMinusOne ++ coordinatesElementAtIndexPlusOne)
+    | otherwise = []
+    where
+        coordinatesElementAtIndexMinusOne = coordinates !! (index - 1)
+        coordinatesElementAtIndexPlusOne = coordinates !! (index + 1)
+        coordinatesElementAtIndex = coordinates !! index
 
-transformCoordinates :: Int -> [(Int, Int)] -> [(Int, Int)]
-transformCoordinates newRowIdx coords =
-    map (\(oldRowIdx, colIdx) -> (newRowIdx, colIdx)) coords
+        check :: (Int, Int) -> [Coordinates] -> [Int]
+        check symbolCoordinate numberCoordinates =
+            let (_, symbolY) = symbolCoordinate
+                validCoordinates = filter (\(Coordinates (_, (coordX, coordY))) -> abs (coordY - symbolY) <= 1 || abs (coordX - symbolY) <= 1) numberCoordinates
+            in
+            if length validCoordinates == 2
+                then map getNumber validCoordinates
+                else []
 
-
-transformListOfCoordinates :: [[(Int, Int)]] -> [[(Int, Int)]]
-transformListOfCoordinates listOfCoords =
-    zipWith transformCoordinates [0..] listOfCoords
 
 main :: IO ()
 main = do
   schematic <- readFileAsLines "./input/2023/day3.txt"
   let exampleInput = ["467..114..","...*......","..35..633.","......#...","617*......",".....+.58.","..592.....","......755.","...$.*....",".664.598.."]
+  --let exampleInput = ["467..114..","...*......","..35..633."]
       extractedNumbers = map extractNumberCoordinates schematic
       extractedSymbols = map extractSymbolsCoordinates schematic
-      extractedSymbolsCoordinates = transformListOfCoordinates extractedSymbols
-      validNumbers = [checkIfNumberAdjacent i coord extractedSymbolsCoordinates | (i, coordinates) <- zip [0..] extractedNumbers, coord <- coordinates]
+      extractedStars = map extractedStarsCoordinates schematic
+      validNumbers = [checkIfNumberAdjacent i coord extractedSymbols | (i, coordinates) <- zip [0..] extractedNumbers, coord <- coordinates]
+      gearRatio = [calcGearRatio i star extractedNumbers  | (i, stars) <- zip [0..] extractedStars, star <- stars]
+      gearRatioValue = sum (map product (filter (not . null) gearRatio))
       sumOfValidNumbers = sum validNumbers
 
 
-  print validNumbers
+  print extractedStars
+  print gearRatio
+  print gearRatioValue
   print sumOfValidNumbers
-  print extractedNumbers
-  print extractedSymbols
-  print extractedSymbolsCoordinates
+
